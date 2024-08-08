b6ece2d4b07ab593dbeda533d5a05a84d30d80a1
Forla03 <fraforla2003@gmail.com>
2024-08-08 09:43:49 +0200

    Iniziato subscriber
    Al subscriber devono arrivare in continuazione i messaggi ogni volta che vengono caricati sul suo topic, qui non l'ho ancora fatto adesso cerco di farlo

--- ProgettoSistemiOperativi/src/progetto/ClientHandler.java
+++ ProgettoSistemiOperativi/src/progetto/ClientHandler.java
@@ -43,11 +43,11 @@ public class ClientHandler implements Runnable {
                         		String key=parts[1];
                             	if(!topic.containsKey(parts[1])) {
                             		topic.add(key);
-                            		to.println("Accesso come Publisher avvenuto con successo. \nIl topic: "+ key+ " non precedentemente esisteste è stato  creato");
+                            		to.println("Accesso come Publisher avvenuto con successo. \nIl topic: "+ key+ " non precedentemente esistente è stato  creato");
                             		gestisciPublisher(key);
                             	}
                             	else {
-                            		to.println("Accesso come Publisher avvenuto con successo. \nIl topic: "+ key + "precedentemente esisteste");
+                            		to.println("Accesso come Publisher avvenuto con successo. \nIl topic: "+ key + "precedentemente esistente");
                             		gestisciPublisher(key);
                             	}
                         	}
@@ -57,7 +57,17 @@ public class ClientHandler implements Runnable {
                         	to.println(allKey);
                         	break;
                         case "subscribe":
-                        	
+                        	if (parts.length > 1) {
+                        		String key=parts[1];
+                            	if(topic.containsKey(parts[1])) {                            		
+                            		to.println("Accesso come Subscriber avvenuto con successo al topic "+key);
+                            		gestisciSubscriber(key);
+                            	}
+                            	else {
+                            		to.println("Accesso come Subscriber fallito, il topic "+ key + " non esiste");                           		
+                            	}
+                        	}
+                        	break;
 
                         default:
                             to.println("Unknown cmd");
@@ -127,7 +137,51 @@ public class ClientHandler implements Runnable {
             e.printStackTrace();
         }
     }
-}
+    
+    public static void gestisciSubscriber(String key) throws InterruptedException{
+    	
+    	try {
+            Scanner from = new Scanner(s.getInputStream());
+            PrintWriter to = new PrintWriter(s.getOutputStream(), true);
+
+            System.out.println("Thread " + Thread.currentThread() + " listening...");
+
+            boolean closed = false;
+            while (!closed) {
+                String request = from.nextLine();
+                if (!Thread.interrupted()) {
+                    System.out.println("Request: " + request);
+                    String[] parts = request.trim().split(" ");
+                    options: switch (parts[0]) {
+                        case "quit":
+                        	
+                            closed = true;
+                            break;
+                            
+                        case "listall":                     
+                        	
+                        	to.println(topic.listAll(key));
+                        	break options;
+                       
+                        default:
+                            to.println("Unknown cmd");
+                    }
+                } else {
+                    to.println("quit");
+                    break;
+                }
+            }
+
+            to.println("quit");
+            s.close();
+            System.out.println("Closed");
+        } catch (IOException e) {
+            System.err.println("ClientHandler: IOException caught: " + e);
+            e.printStackTrace();
+        }
+    }
+    	
+   }
 
     
 
